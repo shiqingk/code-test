0a0dd8be6a60c2e41f18f8460cbf5fc84e95a45d
mzking <978964112@qq.com>
2020-10-13 12:06:07 +0800

    脚本优化

--- 脚本/time.sync.bat
+++ 脚本/time.sync.bat
@@ -1,9 +1,7 @@
 @echo off
+net start w32time
 w32tm /config /manualpeerlist:"time.nist.gov" /syncfromflags:manual /reliable:yes /update
-w32tm /resync
 w32tm /resync
 w32tm /config /manualpeerlist:"time.windows.com" /syncfromflags:manual /reliable:yes /update
 w32tm /resync
-w32tm /resync
-echo 同步结束！
-//pause
+echo OK!
